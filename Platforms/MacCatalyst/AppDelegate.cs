0409cc64b51fdd3bd2bb5f27bda6f4270c32b30a
SzczotkaKamil <szkamil1999@wp.pl>
2023-10-01 12:13:24 +0200

    Add project files.

---
+++ Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace CodeIt
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
