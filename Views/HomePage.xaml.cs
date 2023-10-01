e3d007722b2e74e00b2273e6cf51c34da90f2033
SzczotkaKamil <szkamil1999@wp.pl>
2023-10-01 18:00:55 +0200

    Adjusted the MainPage so it matches the purpose of the Store with Programming Courses App, created a button that redirects to the actual HomePage, cleaned up the solution a bit, by removing irrelevant files/methods.

---
+++ Views/HomePage.xaml.cs
@@ -0,0 +1,9 @@
+namespace CodeIt.Views;
+
+public partial class HomePage : ContentPage
+{
+	public HomePage()
+	{
+		InitializeComponent();
+	}
+}
---
+++ Views/MainPage.xaml.cs
@@ -0,0 +1,16 @@
+﻿namespace CodeIt.Views
+{
+    public partial class MainPage : ContentPage
+    {
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void TapGestureRecognizer_Tapped(object sender, TappedEventArgs e)
+        {
+            await Shell.Current.GoToAsync($"//{nameof(HomePage)}");
+        }
+    }
+}
